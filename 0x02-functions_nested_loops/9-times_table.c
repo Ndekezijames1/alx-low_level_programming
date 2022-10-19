8170f02c63ed8cf96873aac6413f5c2e04c93ac7
Ndekezijames1 <jack.1mick111@gmail.com>
2022-10-19 10:45:53 -0700

    yes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,12 +21,17 @@ prod = num * mult;
  * place the first digit if its two numbers
 */
 if (prod <= 9)
+
 _putchar(' ');
+
 else
-_putchar((prod / 10) + 48); /*get the first digit*/	
+
+_putchar((prod / 10) + 48); /*get the first digit*/
 _putchar((prod % 10) + 48); /*get the second digit*/
 
 }
+
 _putchar('\n');
+
 }
 }
